e5b3ce2e0e2ca7e6459edefdd37f76c918ff3709
Zaiping Ma <zaiping.ma@blackboard.com>
2017-09-01 14:33:23 +0800

    order management

--- xcashweb/src/App.vue
+++ xcashweb/src/App.vue
@@ -1,153 +1,36 @@
 <template>
-  <div id="app">
-    <h1>{{ msg }}</h1>
-    <ul>
-       <p>请选择代付通道:</p>
-      <li>
-	<select v-model="channel">
-		<option disabled value="">请选择代付通道</option>
-		<option value="juzhen_test">钜真测试通道</option>
-		<option value="juzhen">钜真正式通道</option>
-	</select>
-     </li>
-    </ul>
-    <ul>
-      <li>
-	<p>请选择收款商户:</p>
-        <select v-model="store" @change="onSelectedStore($event)">
-       		<option :value="store.id" v-for="store in stores">{{ store.name }}</option>
-	 </select>
-     </li>
-    </ul> 
-    <ul>
-      <li><input type="text" v-model="accountName" id="accountName" placeholder=" 请输入收款人姓名 " /></li>
-    </ul>
-    <ul>
-      <li><input type="text" v-model="cardNum" id="cardNum" placeholder=" 请输入收款账号 " /></li>
-    </ul>
-    <ul>
-      <li><input type="text" v-model="bankName" id="bankName" placeholder=" 请输入开户银行名称 " /></li>
-    </ul> 
-   <ul>
-      <li><input type="text" v-model="totalFee" id="totalFee" placeholder=" 请输入金额 " /></li>
-    </ul>
-    <ul>
-      <li><input type="text" v-model="orderId" id="orderId" placeholder=" 请输入订单号 " /></li>
-    </ul>
-    <ul>
-      <li><input type="button" v-on:click="postCashing"  value="付款"/></li>
-    </ul>
-    <ul>
-      <li><input type="button" v-on:click="getBalance" value="查看帐户余额"/></li>
-    </ul>
-    <ul>
-      <li><input type="button" v-on:click="getOrderStatus"  value="查看代付交易状态"/></li>
-    </ul>
-    <ul>
-      <li>{{ transactionResponse }}</li>
-    </ul>
-  </div>
+  <vue-tabs>
+    <v-tab title="订单管理">
+       <order/>
+    </v-tab>
+ <!--
+    <v-tab title="代付管理">
+      <cash/>
+    </v-tab>
+-->
+    <v-tab title="商户管理">
+       Coming soon...
+    </v-tab>
+
+    <v-tab title="通道管理">
+      Coming soon...
+    </v-tab>
+</vue-tabs>
 </template>
-
-<script src="https://unpkg.com/axios@0.12.0/dist/axios.min.js"></script>
-<script src="https://unpkg.com/lodash@4.13.1/lodash.min.js"></script>
-
 <script>
+import Cash from './Cash.vue'
+import Order from './Order.vue'
+import {VueTabs, VTab} from 'vue-nav-tabs'
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: '代付页面',
-      transactionResponse: '',
-      cardNum: '',
-      accountName: '',
-      bankName: '', 
-      stores: [
-		{id: 0, name: '清空', accountName: '', cardNum: '', bankName: ''},
-		{id: 1, name: '微富支付', accountName: '唐新汀', cardNum: '6217710306687542', bankName: '中信银行深圳民治支行'},
-		{id: 2, name: '钜真测试账号', accountName: '中宏测试', cardNum: '6228480038395977176', bankName: '银行'},
-	]    
-     }
-  },
-
-  methods: {
-	getStore(storeId) {
-            if(!storeId) return ""
-                return this.stores.find( d => d.id === storeId)
-        },
-
-	onSelectedStore(event) {
-        	const storeId = parseInt(event.target.value) 
-		const store = this.getStore(storeId)
-		this.cardNum = store.cardNum
-		this.accountName = store.accountName
-		this.bankName = store.bankName
-        },
-
- 	getBalance() {
-		this.transactionResponse = '提交中......'
-		var vm = this
-		axios.post('http://106.14.47.193/xcash/v1/balance', {channel: this.channel})
-		.then(function (response) {
-		   vm.transactionResponse = response.data
-		})
-		.catch(function (error) {
-            		vm.transactionResponse = '操作失败. ' + error
-          	})
-	 },
-
-	postCashing() {
-                this.transactionResponse = '提交中......'
-                var vm = this
-                axios.post('http://106.14.47.193/xcash/v1/cashing', {channel: this.channel,cardNum:this.cardNum, accountName:this.accountName, bankName:this.bankName, totalFee:this.totalFee,purpose:"代付"})
-                .then(function (response) {
-                   vm.transactionResponse = response.data
-                })
-                .catch(function (error) {
-                        vm.transactionResponse = '操作失败. ' + error
-                })
-         },
-
-	getOrderStatus() {
-                this.transactionResponse = '提交中......'
-                var vm = this
-                axios.post('http://106.14.47.193/xcash/v1/query', {channel: this.channel, orderId:this.orderId})
-                .then(function (response) {
-                   vm.transactionResponse = response.data
-                })
-                .catch(function (error) {
-                        vm.transactionResponse = '操作失败. ' + error
-                })
-         }
-  } 
+    name: 'app',
+    data: {
+    },
+    components: {
+        Cash,
+	Order,
+	VueTabs,
+	VTab
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
---
+++ xcashweb/src/Order.vue
@@ -0,0 +1,170 @@
+<template>
+  <div id="order">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li><input type="text" v-model="targetOrderNo" id="targetOrderNo" placeholder=" 请输入微信／支付宝订单号 " size="40" /></li>
+    </ul>
+    <ul>
+      <li><input type="text" v-model="extOrderNo" id="extOrderNo" placeholder=" 请输入银行订单号 " size="40" /></li>
+    </ul>
+    <ul>
+      <li><input type="text" v-model="orderNo" id="orderNo" placeholder=" 请输入平台订单号 " size="40" /></li>
+    </ul> 
+   <ul>
+      <li><input type="text" v-model="sellerOrderNo" id="sellerOrderNo" placeholder=" 请输入客户订单号 " size="40" /></li>
+    </ul>
+    <ul>
+      <li><input type="button" v-on:click="queryOrder"  value="查询订单"/></li>
+    </ul>
+    <ul>
+      <li><input type="button" v-on:click="refund" value="退款"/></li>
+    </ul> 
+    <ul>
+      <li><input type="button" v-on:click="clear" value="清空"/></li>
+    </ul>
+    <ul>
+      <li>{{ transactionResponse }}</li>
+    </ul>
+    <div id="orderForm" v-if="!this.isBlank(this.order.status)">
+	<table border="1">
+ 	 <tr>
+   	        <th>微信／支付宝订单号</th>
+   	        <th>银行订单号</th>
+		<th>平台订单号</th>
+		<th>客户订单号</th>
+		<th>订单状态</th>
+		<th>交易时间</th>
+		<th>客户名称</th>
+  	</tr>
+  	<tr>
+    		<td>{{order.targetOrderNo}}</td>
+    		<td>{{order.extOrderNo}}</td>
+		<td>{{order.orderNo}}</td>
+		<td>{{order.sellerOrderNo}}</td>
+		<td>{{order.status}}</td>
+		<td>{{order.updateDate}}</td>
+		<td>{{order.storeName}}</td>
+  	</tr>
+	</table>
+    </div>
+  </div>
+</template>
+
+<script src="https://unpkg.com/axios@0.12.0/dist/axios.min.js"></script>
+<script src="https://unpkg.com/lodash@4.13.1/lodash.min.js"></script>
+
+<script>
+export default {
+  name: 'order',
+  data () {
+    return {
+      baseUrl: 'http://106.14.47.193',
+      msg: '订单管理',
+      transactionResponse: '',
+      targetOrderNo: '',
+      extOrderNo: '',
+      order: {orderNo: '',sellerOrderNo: '', status: '',targetOrderNo: '',extOrderNo: '', sellerOrderNo: '', updateDate: '', storeNmae: '', appKey: '', paymentGateway: '', channelNo: ''},
+     }
+  },
+
+  methods: {
+ 	isBlank(str) {
+           return (!str || /^\s*$/.test(str));
+        },
+
+
+	clear() {
+		this.order = {orderNo: '',sellerOrderNo: '', status: '',targetOrderNo: '',extOrderNo: '', sellerOrderNo: '', updateDate: '', storeNmae: '', appKey: '', paymentGateway: '', channelNo: ''}
+		this.transactionResponse = ''
+		this.targetOrderNo = ''
+		this.extOrderNo = ''
+		this.orderNo = ''
+		this.sellerOrderNo = ''	
+	},
+	
+	resetProcessing() {
+		this.transactionResponse = '提交中......'
+		this.order = {orderNo: '',sellerOrderNo: '', status: '',targetOrderNo: '',extOrderNo: '', sellerOrderNo: '', updateDate: '', storeNmae: '', appKey: '', paymentGateway: '', channelNo: ''}
+	},
+
+	queryOrder() {
+		this.resetProcessing()
+		var vm = this
+		var orderUrl = vm.baseUrl + '/xcash/order?'
+		if(!this.isBlank(this.targetOrderNo)) {
+			orderUrl = orderUrl + 'targetOrderNo=' + this.targetOrderNo
+		} else if(!this.isBlank(this.extOrderNo)) {
+			orderUrl = orderUrl + 'extOrderNo=' + this.extOrderNo
+		} else if(!this.isBlank(this.orderNo)) {
+			orderUrl = orderUrl + 'orderNo=' + this.orderNo
+		} else if(!this.isBlank(this.sellerOrderNo)) {
+			orderUrl = orderUrl + 'sellerOrderNo=' + this.sellerOrderNo
+		}
+		axios.get(orderUrl)
+		.then(function (response) {
+		   vm.transactionResponse = '查询成功'
+		   vm.order = response.data
+		})
+		.catch(function (error) {
+            		vm.transactionResponse = '查询失败. ' + error
+          	})
+	 },
+
+	refund() {
+                this.resetProcessing()
+		var vm = this
+		var refundUrl = vm.baseUrl + '/xcash/refund?'
+                if(!this.isBlank(this.targetOrderNo)) {
+                        refundUrl = refundUrl + 'targetOrderNo=' + this.targetOrderNo
+                } else if(!this.isBlank(this.extOrderNo)) {
+                        refundUrl = refundUrl + 'extOrderNo=' + this.extOrderNo
+                } else if(!this.isBlank(this.orderNo)) {
+                        refundUrl = refundUrl + 'orderNo=' + this.orderNo
+                } else if(!this.isBlank(this.sellerOrderNo)) {
+                        refundUrl = refundUrl + 'sellerOrderNo=' + this.sellerOrderNo
+                }
+                axios.delete(refundUrl)
+                .then(function (response) {
+		   if(response.data.status === 'REFUND') {
+                   	vm.transactionResponse = '退款成功'
+                   } else {
+			vm.transactionResponse = '退款失败'
+	     	   }
+		   vm.order = response.data	
+		})
+                .catch(function (error) {
+                        vm.transactionResponse = '退款失败. ' + error
+                })
+         }
+  } 
+}
+</script>
+
+<style>
+#order {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
